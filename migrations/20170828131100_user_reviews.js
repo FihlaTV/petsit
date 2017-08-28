c4c7b2ea6513cca92c856ab7d197b04116f2108a
Natalie Despain <nataliedespainn@gmail.com>
2017-08-28 15:07:20 -0700

    changed user reviews columns

--- migrations/20170828131100_user_reviews.js
+++ migrations/20170828131100_user_reviews.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('user_reviews', function(table){
     table.increments();
     table.integer('owner_id');
-    table.string('user_id');
+    table.integer('user_id');
     table.string('review');
     table.integer('rating');
     table.string('poster_id');
