d5a67af31f737fa36e9801e46db183935d03cb47
Natalie Despain <nataliedespainn@gmail.com>
2017-08-28 12:47:24 -0700

    added users table

---
+++ migrations/20170828124426_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table){
+    table.increments(); 
+    table.string('username');
+    table.string('location');
+    table.string('bio');
+    table.string('email');
+    table.integer('phone');
+    table.string('house_info');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
