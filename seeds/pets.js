924014bd7897f09b0165f74cdf33ca89bc841b03
smckain96 <alexjmckain@gmail.com>
2017-08-28 13:11:31 -0700

    got rid of comma

--- seeds/pets.js
+++ seeds/pets.js
@@ -12,7 +12,7 @@ exports.seed = function(knex, Promise) {
         {owner_id: 4, pet_name: 'haha cow', species: 'fresian', age: 9191919191919191, bio: 'woof i am a cow haha', temperament: 'bitter', notes: 'wanted in 3 countries for illegal grass trafficking' },
         {owner_id: 5, pet_name: 'haha whale', species: 'blue whale', age: 9191919191919191, bio: 'woof i am a whale haha', temperament: 'sweet', notes: 'wanted in 3 countries for illegal krill trafficking' },
         {owner_id: 6, pet_name: 'haha goat', species: 'idek tbh', age: 9191919191919191, bio: 'woof i am a goat haha', temperament: 'sour', notes: 'wanted in 3 countries for illegal whatever goats eat trafficking' },
-        {owner_id: 7, pet_name: 'haha horse', species: 'big horse', age: 9191919191919191, bio: 'woof i am a horse haha', temperament: 'horsy', notes: 'wanted in 3 countries for illegal carrot trafficking' },
+        {owner_id: 7, pet_name: 'haha horse', species: 'big horse', age: 9191919191919191, bio: 'woof i am a horse haha', temperament: 'horsy', notes: 'wanted in 3 countries for illegal carrot trafficking' }
       ]);
     });
 };
