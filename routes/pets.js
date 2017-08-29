e68d4388f6a45d5f2340c987234dbf337c73df65
Dylan Cunningham <gdylanc@gmail.com>
2017-08-29 13:59:49 -0700

    pet add

--- routes/pets.js
+++ routes/pets.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
 
+
+// add yo pet
 router.get('/add', function(req, res, next) {
   if(req.cookies.user_id){
       res.render('petAdd', { cookie: req.cookies.user_id });
@@ -18,7 +20,9 @@ router.post('/add', function(req, res, next){
 })
 
 
-/* GET home page. */
+
+
+// show pet
 router.get('/:id', function(req, res, next) {
   knex.raw(`select pets.id as id, pet_name, species,age, pets.bio as bio, temperament, pic_url, username, notes, location from pets join users on pets.owner_id = users.id WHERE pets.id = ${req.params.id}`)
   .then(function(pets){
@@ -27,12 +31,16 @@ router.get('/:id', function(req, res, next) {
 });
 
 
+
+// edit a pet
 router.get('/:id/edit', function(req, res, next) {
     knex.raw(`select * from pets WHERE pets.id = ${req.params.id}`)
     .then(function(pets){
       res.render('petEdit', { pet:pets.rows[0] })
     })
 });
+
+
 
 router.post('/:id/edit', function(req, res, next) {
     knex.raw(`update pets set pet_name='${req.body.name}', temperament='${req.body.temperament}', bio = '${req.body.bio}', age= ${req.body.age}, notes = '${req.body.notes}', pic_url = '${req.body.pic_url}' WHERE id = ${req.params.id}`)
@@ -46,17 +54,18 @@ router.post('/:id/edit', function(req, res, next) {
 
 
 
-
-router.get('/:id/delete', function(req, res, next) {
-  res.redirect('/home');
-
-
+// delete a pet
+router.post('/:id/delete', function(req, res, next) {
+  knex.raw(`delete from pets where id = ${req.params.id}`)
+  .then(function(data){
+    res.redirect(`/users/${req.cookies.user_id}`)
+  })
 });
 
 
 
 // petDelete
-// petAdd
+
 
 
 
