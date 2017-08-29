07bf4b6058917ca35742f09ff770cd565e076b21
smckain96 <alexjmckain@gmail.com>
2017-08-29 10:17:09 -0700

    Finished show route for pets

--- routes/pets.js
+++ routes/pets.js
@@ -5,20 +5,15 @@ var knex = require('../db/knex.js');
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-  knex.raw(`select * from pets join users on pets.owner_id = users.id WHERE pets.id = ${req.params.id}`)
+  knex.raw(`select pet_name, species,age, pets.bio as bio, temperament, pic_url, username, notes, location from pets join users on pets.owner_id = users.id WHERE pets.id = ${req.params.id}`)
   .then(function(pets){
     res.render('petShow', { pets: pets.rows[0] });
-    console.log(pets.rows.pet_name);
   })
-
-
 });
 
 
 router.get('/:id/edit', function(req, res, next) {
   res.render('petEdit', { title: 'Express' });
-
-
 });
 
 
