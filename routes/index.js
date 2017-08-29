419b143f0f3519724ba1fd482152af00487b047e
Natalie Despain <nataliedespainn@gmail.com>
2017-08-29 09:55:49 -0700

    added login and edit user functionality

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,30 @@ var bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-
-
+  res.render('index', {cookies: req.cookies.user_id});
 });
 
 // create account
-router.get('/n')
+router.post('/login', function(req, res, next) {
+	console.log(req.body.login)
+	knex.raw(`SELECT * FROM users where username = '${req.body.login}'`)
+  	.then(user => {
+  		console.log(user.rows[0])
+  		bcrypt.compare(req.body.password, user.rows[0].password, (err, resp) => {
+  			if (resp) {
+  				res.cookie('user_id', user.rows[0].id);
+					res.redirect('/');
+  			} else {
+  				res.send('try again');
+  			}
+  		});
+  	});
+});
+
+router.get('/logout', (req, res, next) => {
+  res.clearCookie('user_id');
+	res.redirect('/');
+});
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,33 @@ var bcrypt = require('bcrypt');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 // create new user
 router.get('/new', function(req, res, next) {
   res.render('newUser')
 })
 
-router.post('/create/', function(req, res, next) {
+// edit page
+router.get('/:id/edit', function(req,res, next){
+  knex.raw(`SELECT * FROM users WHERE id = ${req.params.id}`)
+    .then(function(user) {
+      res.render('editUser', {user: user.rows[0]})
+    });
+})
+
+// show user page
+router.get('/:id', function(req, res, next){
+  knex.raw(`SELECT users.*, pets.id as pet_id, pets.pet_name, pets.species, pets.pic_url FROM users JOIN pets ON users.id = pets.owner_id WHERE users.id = ${req.params.id}`)
+  .then(function(data){
+    knex.raw(`SELECT user_reviews.*, users.username FROM user_reviews JOIN users ON users.id = user_reviews.poster_id WHERE user_id = ${req.params.id}`)
+      .then(function(reviews) {
+        res.render('dashboard', {user_info: data.rows, reviews: reviews.rows})
+      })
+  })
+})
+
+// create user
+router.post('/add', function(req, res, next) {
   if (req.body.password === req.body.verify) {
     bcrypt.hash(req.body.password, 8, function(err, hash) {
       knex.raw(`INSERT INTO users VALUES (default, '${req.body.username}', '${hash}', '${req.body.location}', '${req.body.bio}', '${req.body.email}', '${req.body.phone}', '${req.body.info}')`)
@@ -24,19 +45,11 @@ router.post('/create/', function(req, res, next) {
   }
 })
 
-router.get('/edit/', function(req,res, next){
-  res.send("edit")
-})
-
-// show user page
-router.get('/:id', function(req, res, next){
-  knex.raw(`SELECT users.*, pets.id as pet_id, pets.pet_name, pets.species, pets.pic_url FROM users JOIN pets ON users.id = pets.owner_id WHERE users.id = ${req.params.id}`)
-  .then(function(data){
-    knex.raw(`SELECT user_reviews.*, users.username FROM user_reviews JOIN users ON users.id = user_reviews.poster_id WHERE user_id = ${req.params.id}`)
-      .then(function(reviews) {
-        res.render('dashboard', {user_info: data.rows, reviews: reviews.rows})
-      })
-  })
+router.post('/:id', function(req, res, next) {
+  knex.raw(`UPDATE users SET (username, location, bio, email, phone_number, house_info) = ('${req.body.username}', '${req.body.location}', '${req.body.bio}', '${req.body.email}', '${req.body.phone_number}', '${req.body.info}') WHERE id = ${req.params.id}`)
+    .then(function(user) {
+      res.redirect(`/users/${req.params.id}`)
+    })
 })
 
 
