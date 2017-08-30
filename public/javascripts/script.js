2ab0fe4b5da160192621f51703ad74f3085fb789
MacRJ <rmacwj@gmail.com>
2017-08-30 09:36:15 -0700

    basic grid added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-  $(".add-comment").submit(function(e) {
-    console.log("submitted")
-    var user = $("#comment-user")
-    var comment = $("#comment").val();
-    console.log(comment);
-    console.log(user)
-    $(".content").append("<div>" + user + ":" + comment + "</div>");
-    $("#comment").children('input').val('');
-  })
+  console.log("i'm up")
+
+
+
+
+
+
 
 })
